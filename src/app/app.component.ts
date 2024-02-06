472556296aec855e2a1af30f0b3147200572b848
Dinesh yadav <dineshyadav74950@gmail.com>
2024-02-06 12:12:21 +0530

    Date Percent Decimal Currency in Angular 16

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DatePercentCurrencyInAngular16';
+  currentDate = new Date();
+  mynumber : number =0.23456788;
+  
+}
+
